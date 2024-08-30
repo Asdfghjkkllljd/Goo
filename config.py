150487051dd5f8f9f83ceb1def1b30e8d7adb89e
Asdfghjkkllljd <staricinal49@gmail.com>
2024-08-30 13:39:32 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
-TOKEN = "7429518608:AAFMU2R_CyyLqPhWl2LJTWxTQVPU1DhSppk" # Токен бота
-CHANNEL_ID = -1002243436081 # ID канала со ставками
-ADMINS = [5846969551] # ID админов
-BET_URL = "t.me/send?start=IVpAqLJ8Pztg" # Ссылка на счет
-CRYPTOPAY_TOKEN = "251801:AAaOA42R6B4OjqU4ciwBt2KRjmAHfyPx6mY" # Тут CryptoPay токен
+TOKEN = "6945780105:AAGrDeU2G8eThmKnlf_0oCpFvVdSrIgRK_c" # Токен бота
+CHANNEL_ID = -1002056388271 # ID канала со ставками
+ADMINS = [6372950353] # ID админов
+BET_URL = "http://t.me/send?start=IV7At7pCRjY0" # Ссылка на счет
+CRYPTOPAY_TOKEN = "260414:AAT42GCmdNAYFFWsb39Qj8n3qb61Hfes5oa" # Тут CryptoPay токен
 WIN_IMAGE = "win.jpg" # Название файла с картинкой выигрыша
 LOSE_IMAGE = "lose.jpg" # Название файла с картинкой проигрыша
-LOGS_ID = -1002226990800 # ID канала с оплатами
+LOGS_ID = -1002241301442 # ID канала с оплатами
 
-RULES_LINK = "https://t.me/+k42zVBd8Iz41ZDEy" # Ссылка на канал с правилами
-NEWS_LINK = "https://t.me/+YXO5t3AYIRgxZTdi" # Ссылка на новостной канал
-OWNER_LINK = "https://t.me/CEKC_HA_OCTPOBAX" # Ссылка на владельца/поддержку
-PEREHOD_LINK = "@extremecasinoo" # Ссылка на переходник
+RULES_LINK = "https://t.me/FLIPPY_PRAVILA" # Ссылка на канал с правилами
+NEWS_LINK = "https://t.me/flippyNOVOSTI" # Ссылка на новостной канал
+OWNER_LINK = "https://t.me/official_neender" # Ссылка на владельца/поддержку
+PEREHOD_LINK = "https://t.me/Flippy_Casino" # Ссылка на переходник
 BETS_LINK = "https://t.me/extremecasinoo" # Ссылка на канал ставок
-CASINO_NAME = "EXTREME CASINO" # Название казино
+CASINO_NAME = "FLIPPY CASINO" # Название казино
